ea99c8fb538913f7566b2a304741f1f03c801343
sejal924 <sejalchouhan24@gmail.com>
2025-02-27 15:58:27 +0530

    [Sejal]Add.Using lombok library for logging

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.employeepayrollapp;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
+		 final Logger log = (Logger) LoggerFactory.getLogger(EmployeePayrollAppApplication.class);
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started");
 	}
 	
 }
