af36cf79aff30521bd10e125b5cc1bd6309edbaa
sejal924 <sejalchouhan24@gmail.com>
2025-02-27 14:52:30 +0530

    [Sejal]Add.Using lombok library to auto generate getter and setter

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,22 +1,12 @@
 package com.bridgelabz.employeepayrollapp.dto;
+import lombok.Data;
 
-public class EmployeeDTO {
+public @Data class EmployeeDTO {
     private String name;
     private double salary;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
+    
+    public EmployeeDTO(String name,double salary) {
+    	this.name = name;
+    	this.salary = salary;
     }
 }
