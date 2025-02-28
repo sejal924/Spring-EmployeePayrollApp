b7c595fec8ee24257981775afb3321225ebf1d8b
sejal924 <sejalchouhan24@gmail.com>
2025-02-28 14:22:00 +0530

    [Sejal]Add.Determine logging level,logging pattern,logging to console or file

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,7 +3,7 @@ package com.bridgelabz.employeepayrollapp;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
 import ch.qos.logback.classic.Logger;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,9 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		 final Logger log = (Logger) LoggerFactory.getLogger(EmployeePayrollAppApplication.class);
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		 
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				 context.getEnvironment().getProperty("environment"));
 	}
 	
 }
